526585f3a293d3f3e27581f1a740d3233191a605
igorduartetkd <igorduartetkd@gmail.com>
2022-08-07 12:38:41 -0300

    Criando objeto de serviços para simular a resposta da api

---
+++ src/dataSources/servicos.js
@@ -0,0 +1,166 @@
+
+
+
+const servicos = [
+    {
+        "titulo": "Transfer",
+        "Descricao": "Buscar e levar ao aeroporto ou para a sua nova casa.",
+        "precos": [
+            {
+                "categoria": "distancia",
+                "unidade": "km",
+                "valor": 1.50,
+                "qtd_minima": 20,
+                "moeda": "C$",
+            },
+            {
+                "categoria": "tempo",
+                "unidade": "h",
+                "valor": 40.00,
+                "qtd_minima": 1,
+                "moeda": "C$"
+            },
+        ],
+    },
+    {
+        "titulo": "Compras",
+        "Descricao": "Compras no mercado antes de sua chegada para que tenha pelo menos o básico na sua casa.",
+        "precos": [
+            {
+                "categoria": "distancia",
+                "unidade": "km",
+                "valor": 1.50,
+                "qtd_minima": 20,
+                "moeda": "C$",
+            },
+            {
+                "categoria": "tempo",
+                "unidade": "h",
+                "valor": 40.00,
+                "qtd_minima": 1,
+                "moeda": "C$"
+            },
+        ],
+    },
+    {
+        "titulo": "Cambio",
+        "Descricao": "Ajuda com troca/cambio de dinheiro + cartão de crédito (Algumas compras iniciais tem que ter cartão de crédito aqui do Canadá, para isso usaremos noso cartão se for preciso).",
+        "precos": [
+            {
+                "categoria": "unidade",
+                "unidade": "ud",
+                "valor": 0.00,
+                "qtd_minima": 0,
+                "moeda": "C$"
+            },
+        ],
+    },
+    {
+        "titulo": "Busca de chaves",
+        "Descricao": "Buscamos as chaves de seu imóvel para caso chegue fora do horário comercial não precise se preocupar em marcar com os proprietários para isso.",
+        "precos": [
+            {
+                "categoria": "distancia",
+                "unidade": "km",
+                "valor": 1.50,
+                "qtd_minima": 20,
+                "moeda": "C$",
+            },
+        ],
+    },
+    {
+        "titulo": "Limpeza",
+        "Descricao": "Limpamos o seu novo lar antes da sua chegada para evitar problemas como a alergia da poeira.",
+        "precos": [
+            {
+                "categoria": "distancia",
+                "unidade": "km",
+                "valor": 1.50,
+                "qtd_minima": 20,
+                "moeda": "C$",
+            },
+            {
+                "categoria": "tempo",
+                "unidade": "h",
+                "valor": 40.00,
+                "qtd_minima": 1,
+                "moeda": "C$"
+            },
+        ],
+    },
+    {
+        "titulo": "Visita em apê",
+        "Descricao": "Visitamos o local onde desejam morar, com um olhar de engenheiro, dizemos nossa opnião sobre a qualidade do imóvel. Além de evitar riscos de serem enganados com propagandas falsas.",
+        "precos": [
+            {
+                "categoria": "distancia",
+                "unidade": "km",
+                "valor": 1.50,
+                "qtd_minima": 20,
+                "moeda": "C$",
+            },
+            {
+                "categoria": "tempo",
+                "unidade": "h",
+                "valor": 40.00,
+                "qtd_minima": 1,
+                "moeda": "C$"
+            },
+        ],
+    },
+    {
+        "titulo": "Montagem e transporte",
+        "Descricao": "Transporte e montagem de móveis para que você chegue com tudo montado e em seu lugar.",
+        "precos": [
+            {
+                "categoria": "distancia",
+                "unidade": "km",
+                "valor": 1.50,
+                "qtd_minima": 20,
+                "moeda": "C$",
+            },
+            {
+                "categoria": "tempo",
+                "unidade": "h",
+                "valor": 40.00,
+                "qtd_minima": 1,
+                "moeda": "C$"
+            },
+        ],
+    },
+    {
+        "titulo": "Dia para regularização",
+        "Descricao": "Te levamos e orientamos para realizar as atividades burocráticas como o NAS, criar sua conta no banco, fazer o seu plano de celular.",
+        "precos": [
+            {
+                "categoria": "distancia",
+                "unidade": "km",
+                "valor": 1.50,
+                "qtd_minima": 20,
+                "moeda": "C$",
+            },
+            {
+                "categoria": "tempo",
+                "unidade": "h",
+                "valor": 40.00,
+                "qtd_minima": 1,
+                "moeda": "C$"
+            },
+        ],
+    },
+    {
+        "titulo": "Costco day",
+        "Descricao": "Hora de conhecer o melhor lugar para fazer suas compras, mais emição do seu cartão de membro.",
+        "precos": [
+            {
+                "categoria": "tempo",
+                "unidade": "h",
+                "valor": 40.00,
+                "qtd_minima": 1,
+                "moeda": "C$"
+            },
+        ],
+    },
+]
+
+export default servicos;
